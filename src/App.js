60e69c0b48999f13b7f7c0f76bb52fc5ab7c1bdb
Samee Rauf <samee.rauf@pmg.com>
2025-01-03 12:46:36 -0800

    added gallyery model

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
+import React from 'react';
+import '@google/model-viewer/dist/model-viewer';
 
-function App() {
+export default function App() {
+  const modelStyle = {
+    position: 'absolute',
+    top: '10px',
+    right: '10px',
+    width: '150px',
+    height: '150px',
+    zIndex: 0,
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Gallery</h1>
+        <h1>Gallery</h1>
+        <h2>3D Model Viewer</h2>
+        <model-viewer
+          src="/Polaroids.glb"
+          alt="Computer Model"
+          auto-rotate={true} // Enable auto-rotation
+          interaction-prompt="none" // Remove drag hand prompt
+          disable-zoom={true} // Prevent zooming
+          rotation-per-second="30deg" // Spin clockwise
+          style={modelStyle} // Add inline styles
+        ></model-viewer>
       </header>
     </div>
   );
 }
-
-export default App;
