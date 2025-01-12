d4ff9303065deb9e15e4d6077796fc483e830062
Samee Rauf <samee.rauf@pmg.com>
2025-01-12 09:32:23 -0600

    added form brackets

--- src/Components/InputForm.js
+++ src/Components/InputForm.js
@@ -3,9 +3,11 @@ import React from 'react';
 const InputForm = ({ title, description }) => {
   return (
     <div className="input-form">
+        <form>
       <h3 className="input-form-title">{title}</h3>
       <input className="input-form-text" type="text" placeholder="Input this attribute" />
       <p className="input-form-paragraph">{description}</p>
+        </form>
     </div>
   );
 };
