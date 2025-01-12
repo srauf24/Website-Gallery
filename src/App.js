42e3b3f9358f73e31843b250c1cf60caff469de7
Samee Rauf <samee.rauf@pmg.com>
2025-01-12 09:58:28 -0600

    formated code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import React from 'react';
-import {useState} from "react"
+import React, { useState } from 'react';
 import '@google/model-viewer/dist/model-viewer';
 import InputForm from './Components/InputForm';
-import * as querystring from "node:querystring";
-const ACCESS_KEY = process.env.REACT_APP_ACCESS_KEY;
+import * as querystring from 'node:querystring';
 
 export default function App() {
   const modelStyle = {
@@ -14,51 +12,55 @@ export default function App() {
     width: '150px',
     height: '150px',
     zIndex: 0,
-
   };
+
   const [inputs, setInputs] = useState({
-  url: "",
-  format: "",
-  no_ads: "",
-  no_cookie_banners: "",
-  width: "",
-  height: "",
-});
-const submitForm = (e) => {
-  e.preventDefault(); // Prevent the default form submission behavior
+    url: '',
+    format: '',
+    no_ads: '',
+    no_cookie_banners: '',
+    width: '',
+    height: '',
+  });
 
-  let defaultValues = { // just in case input values are empty
-    format: "jpeg",
-    no_ads: "true",
-    no_cookie_banners: "true",
-    width: "1920",
-    height: "1080",
+  const submitForm = (e) => {
+    e.preventDefault(); // Prevent the default form submission behavior
+
+    let defaultValues = {
+      format: 'jpeg',
+      no_ads: 'true',
+      no_cookie_banners: 'true',
+      width: '1920',
+      height: '1080',
+    };
+
+    if (!inputs.url.trim()) {
+      alert('Please enter a URL.');
+      return;
+    } else {
+      for (const [key, value] of Object.entries(inputs)) {
+        if (value === '') {
+          inputs[key] = defaultValues[key];
+        }
+      }
+      makeQuery();
+    }
   };
 
-  if (!inputs.url.trim()) {
-    alert("Please enter a URL.");
-    return;
-  } else {
-    for (const [key, value] of Object.entries(inputs)) {
-      if (value === "") {
-        inputs[key] = defaultValues[key];
-      }
-    }
-    makeQuery();
-  }
   const makeQuery = () => {
-    let wait_until = "network_idle";
-    let response_type = "json";
-    let fail_on_status = "400%2C404%2C500-511";
-    let url_starter = "https://";
+    let wait_until = 'network_idle';
+    let response_type = 'json';
+    let fail_on_status = '400%2C404%2C500-511';
+    let url_starter = 'https://';
     let fullURL = url_starter + inputs.url;
     let query = `https://api.apiflash.com/v1/urltoimage?access_key=${ACCESS_KEY}&url=${fullURL}&format=${inputs.format}&width=${inputs.width}&height=${inputs.height}&no_cookie_banners=${inputs.no_cookie_banners}&no_ads=${inputs.no_ads}&wait_until=${wait_until}&response_type=${response_type}&fail_on_status=${fail_on_status}`;
-  // Add your form submission logic here
-};
+    // Add your form submission logic here
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className={"title"}>Build Your Own Screenshot 📸</h1>
+        <h1 className="title">Build Your Own Screenshot 📸</h1>
         <model-viewer
           src="/Deer.glb"
           alt="Computer Model"
@@ -70,16 +72,15 @@ const submitForm = (e) => {
         ></model-viewer>
         <InputForm
           inputs={inputs}
-        handleChange={(e) =>
-          setInputs((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value.trim(),
-          }))
-        }
-        onSubmit={submitForm}
-      />
-      <br></br>
-
+          handleChange={(e) =>
+            setInputs((prevState) => ({
+              ...prevState,
+              [e.target.name]: e.target.value.trim(),
+            }))
+          }
+          onSubmit={submitForm}
+        />
+        <br />
       </header>
     </div>
   );
